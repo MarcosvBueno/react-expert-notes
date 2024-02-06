bd5a0143c80151542bb9b220937b9dbfdd5ef1ff
MarcosvBueno <mvbueno07@gmail.com>
2024-02-06 16:00:28 -0300

    feat(main): adding basic application layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,29 @@
-import logoNLW from './assets/Logo-nlw-expert.svg'
+import logoNLW from "./assets/Logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
 
 export function App() {
-
   return (
-    <>
-    <img src={logoNLW} alt="" />
-    </>
-  )
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logoNLW} alt="logo " />
 
+      <form action="" className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-2xl font-semibold tracking-tight placeholder:text-slate-500 focus:outline-none"
+        />
+      </form>
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+      <NewNoteCard />
+      <NoteCard />
+      <NoteCard />
+      <NoteCard />
+
+
+      </div>
+    </div>
+  );
 }
-
-
