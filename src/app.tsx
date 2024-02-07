6e5aaa80e014f0e43586fef78cb3719420943cdb
MarcosvBueno <mvbueno07@gmail.com>
2024-02-07 12:52:31 -0300

    feat(main): adding modal layout

--- src/app.tsx
+++ src/app.tsx
@@ -17,12 +17,13 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-      <NewNoteCard />
-      <NoteCard />
-      <NoteCard />
-      <NoteCard />
-
-
+        <NewNoteCard />
+        <NoteCard
+          note={{
+            date: new Date(),
+            content: "Aqui está o conteúdo da nota",
+          }}
+        />
       </div>
     </div>
   );
